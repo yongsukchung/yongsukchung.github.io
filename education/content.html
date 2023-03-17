923a088f4b457e4d6e099024c1eb0662160ad44e
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-03-17 12:39:54 +0700

    refactor: Update index.html and add content.html for route

---
+++ education/content.html
@@ -0,0 +1,1 @@
+<div>Education</div>
--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 $(function () {
-    $("#primaryNav").load("/components/common/primaryNav.html")
-    $("#secondaryNav").load("/components/home/secondaryNav.html")
-    $('#pageContent').load('pages/home.html');
-    $('#primaryNav').on('click', 'ul li a', function (event) {
-        event.preventDefault();
+    $("#primaryNav").load("../../common/primaryNav.html")
+    $("#secondaryNav").load("secondaryNav.html")
+    $('#footer').load('../../common/footer.html')
+    // $('#brandingOther').load('images/personnelbranding.jpg')
+    $('#pageContent').load('content.html');
+    // $('#primaryNav').on('click', '.nav-link', function (event) {
+    //     event.preventDefault()
 
-        var href = $(this).attr('href');
+    //     var href = $(this).attr('href')
 
-        $('#pageContent').load(`components/'${href}/page.html`);
-        $('#secondaryNav').load(`components/${href}/secondaryNav.html`);
-    });
-
+    //     $('#pageContent').load(`components/${href}/page.html`)
+    //     $('#secondaryNav').load(`components/${href}/secondaryNav.html`)
+    // });
+    
 })
