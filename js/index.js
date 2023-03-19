20b246bdfc0895aa78d05c8b71673dd2ede521ca
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-03-19 17:01:07 +0700

    feat: Add content for Personnel, Collaborators, Education

--- js/index.js
+++ js/index.js
@@ -1,26 +1,35 @@
 $(function () {
 
-    let themeInLocalStorage = window.localStorage.getItem('theme')
-    let theme = (themeInLocalStorage === null) ? 'superhero' : themeInLocalStorage
+    // Load theme css
+    let theme = window.localStorage.getItem('theme') || 'superhero';
     $('head').append(`
-    <link href="../../css/${theme}-bootstrap.min.css" rel="stylesheet" id="theme-css" />
+        <link href="../../css/${theme}-bootstrap.min.css" rel="stylesheet" id="theme-css" />
     `)
 
+    // Load footer
     $('#footer').load('../../common/footer.html')
-    let path = window.location.pathname.replace("index.html", "").split('/').filter(Boolean)
     
-    if (path.length === 0) path.push('home')
-
+    // Load primary navigation bar
     $("#primaryNav").load("../../common/primaryNav.html", () => {
         let path = window.location.pathname.replace("index.html", "").split('/').filter(Boolean)
         $(`#nav-link-${path[0]}`).addClass('active')
     })
 
+    let path = window.location.pathname.replace("index.html", "").split('/').filter(Boolean)
+    if (path.length === 0) path.push('home')
+    
+    // Load secondary navigation bar
+    $("#secondaryNav").load(`../../${path[0]}/secondaryNav.html`)
+    
     path = path.join('/')
-    $("#secondaryNav").load(`../../${path}/secondaryNav.html`)
-    $("#brandingOther").addClass('container-fluid')
-    $("#brandingOther").html('<img src="../../images/personnelbranding.jpg" alt="brading-banner"/>')
+    // Loading branding
+    $("#brandingOther")
+    .html('<img src="../../images/personnelbranding.jpg" alt="brading-banner"/>')
+
+    // Loading content
     $('#content').load(`../../${path}/content.html`)
+
+    // Prevent reload in when navigate to same page
     $('#primaryNav').on('click', '.nav-link', function (event) {
         let href = $(this).attr('href').split('/').filter(Boolean)[0]
         if (href === path) event.preventDefault()
