da9f2988285941f25df4dc7d8ab8c4d0b7ee762d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-03-20 17:31:35 +0700

    refactor: Update index.html and index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 $(function () {
 
+    $('body').append(`
+        <div id="mainContainer">
+            <div id="brandingLogo"></div>
+            <div id="primaryNav"></div>
+            <div id="brandingOther">
+            </div>
+            <div class="row m-5">
+                <div id="secondaryNav" class="col-2 container"></div>
+                <div id="content" class="col-10"></div>
+            </div>
+            <div id="footer" class="d-flex p-2"></div>
+        </div>
+    `)
+    .append(`
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+    `)
+
+
     // Load theme css
     let theme = window.localStorage.getItem('theme') || 'superhero';
     $('head').append(`
@@ -8,7 +28,7 @@ $(function () {
 
     // Load footer
     $('#footer').load('../../common/footer.html')
-    
+
     // Load primary navigation bar
     $("#primaryNav").load("../../common/primaryNav.html", () => {
         let path = window.location.pathname.replace("index.html", "").split('/').filter(Boolean)
@@ -17,14 +37,14 @@ $(function () {
 
     let path = window.location.pathname.replace("index.html", "").split('/').filter(Boolean)
     if (path.length === 0) path.push('home')
-    
+
     // Load secondary navigation bar
     $("#secondaryNav").load(`../../${path[0]}/secondaryNav.html`)
-    
+
     path = path.join('/')
     // Loading branding
     $("#brandingOther")
-    .html('<img src="../../images/personnelbranding.jpg" alt="brading-banner"/>')
+        .html('<img src="../../images/personnelbranding.jpg" alt="brading-banner"/>')
 
     // Loading content
     $('#content').load(`../../${path}/content.html`)
