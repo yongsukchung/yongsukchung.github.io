1f54f8a4385aef1240bb39dc01eea08a31f997b9
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-07-02 09:36:32 +0700

    refactor: Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,18 @@
+const API_KEY = "AIzaSyAxOmuGx3b5PU514LzThHv6OhEEP5fX-sA";
+
 function getFile(fileId) {
-  const API_KEY = "AIzaSyAxOmuGx3b5PU514LzThHv6OhEEP5fX-sA";
   return new Promise((resolve, reject) => {
     fetch(`https://www.googleapis.com/drive/v2/files?q='${fileId}'+in+parents&key=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => reject('Error retrieving files:', error));
+  });
+}
+function getFileDetails(fileId) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://www.googleapis.com/drive/v2/files/${fileId}?key=${API_KEY}`)
       .then(response => response.json())
       .then(data => {
         resolve(data);
